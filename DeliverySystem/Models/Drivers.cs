56499c85b2a055f202a4f3686c6089c7c9c5a69f
Atmar Kohistany <atma0004@edu.eal.dk>
2017-11-06 12:58:56 +0100

    Add project files.

---
+++ DeliverySystem/Models/Drivers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DeliverySystem.Models
+{
+    public class Drivers
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public bool OnDuty  { get; set; }
+        public bool Availability { get; set; }
+        List<Order> Orders = new List<Order>();
+
+    }
+}
---
+++ DeliverySystem/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿namespace DeliverySystem.Models
+{
+    internal class Order
+    {
+        public int ID { get; set; }
+        public string Adderss { get; set; }
+        public int PostCode { get; set; }
+        public int DoorNumber { get; set; }
+        public enum Floor{ Basement , Groundfloor ,Firstfloor ,SecondFloor,ThirdFloor,FourthFloor,FifthFloor };
+        public enum FloorSide {tv ,th , mdf }
+        public bool IsPaied { get; set; }
+        public enum StstusOfTheDelivery { OnTheWay,Deliverd}
+        public string Time { get; set; }
+
+    }
+}
